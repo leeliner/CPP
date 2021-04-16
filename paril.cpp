a2bd58ec79a7ec5989876820af976874625ad252
leeliner <leeliner@sina.com>
2021-04-16 10:37:41 +0000

    rewrite paril 2021.4.16 author by JY;

--- paril.cpp
+++ paril.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <iomanip>
+using namespace std;
 int main()
-{int a,b,day=1;
-    cin>>a;
+{int a=1,b,day=1;
+    //cin>>a;
     int a1=a;
     while(a1>0)
     {cout<<"   ";
-    a--;}
+    a1--;}
     for (;a<8;a++)
     {
-        cout <<setw(2)<<day++ " ";
+        cout <<setw(2)<<day++ <<" ";
 
     }
-    
+
     
     cout <<endl;
 for (a=1;a<5;a++)
 {
-    for(int j=0;(j<7)&&(day<32;j++)
+    for(int j=0;(j<7)&&(day<32);j++)
     {if(day>9)
-    cout<<day++;
-    else cout <<setw(2)<<day++;}
+    cout<<day++<<" ";
+    else cout <<setw(2)<<day++<<" ";}
     cout <<endl;
 }
     
