dbcac73666f3172584579a3494988f927bb43a0e
leeliner <59076056+leeliner@users.noreply.github.com>
2021-04-18 18:59:21 +0800

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -1,43 +1,47 @@
 using System;
-public class book{
-    string bookname ;
-    int price;
-    string author ;
-    public book(){
-
-        Console.WriteLine(" 无参构造函数被调用");
-    }
-    public  void setbookname (string n ){
-        bookname =n ;
-        
-    }
-    public void setprice (int p ){
-        price =p ;
-    }
-    public void setauthor (string a)
-    {
-        author=a;
-    }
-    
-    public book(string bookname,int price,string author)
-    {
-        this.bookname =bookname;
-        this.price=price;
-        this.author=author;
-        Console.Write("有参构造函数被调用");
-    }
-    static void main(string[] args)
+public class book
 {
-   
-    book a=new book();
-    a.setauthor("YJ-L");
-    a.setprice("99.9");
-    a.setbookname("D#");
-    book b= new book("C#","9.9","JY-L");
+    string bookname;
+    int price;
+    string author;
+    public book()
+    {
+
+        Console.WriteLine("无参构造函数被调用");
+    }
+    public void setbookname(string n)
+    {
+        bookname = n;
+
+    }
+    public void setprice(int p)
+    {
+        price = p;
+    }
+    public void setauthor(string a)
+    {
+        author = a;
+    }
+
+    public book(string bookname, int price, string author)
+    {
+        this.bookname = bookname;
+        this.price = price;
+        this.author = author;
+        Console.WriteLine("有参构造函数被调用");
+    }
+    static void Main(string[] args)
+    {
+
+        book a = new book();
+        a.setauthor("YJ-L");
+        a.setprice(99);
+        a.setbookname("D#");
+        book b = new book("C#", 999,"JY-L");
+        Console.WriteLine(a.author);
+        Console.WriteLine(b.author);
 
 
-  }
-    
+    }
+
 }
-
-
